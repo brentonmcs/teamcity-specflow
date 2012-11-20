d35b92d6040d774704066a81a93fd35d61528470
Graham Hay <grahamrhay@gmail.com>
2012-11-20 16:32:03 +0000

    Restart agent when installing plugin

--- install-plugin.ps1
+++ install-plugin.ps1
@@ -1,6 +1,9 @@
 Write-Host "Stopping TC service"
 Stop-Service teamcity
 
+Write-Host "Stopping TC agent"
+Stop-Service tcbuildagent
+
 $pluginFolder = "C:\ProgramData\JetBrains\TeamCity\plugins"
 Write-Host "Purging plugin folder"
 Get-ChildItem $pluginFolder | Remove-Item -Recurse
@@ -11,6 +14,9 @@ Copy-Item ".\dist\specFlowPlugin.zip" $pluginFolder
 $logFolder = "C:\TeamCity\logs"
 Write-Host "Cleaning log folder"
 Get-ChildItem $logFolder | Remove-Item
+
+Write-Host "Starting TC agent"
+Start-Service tcbuildagent
 
 Write-Host "Starting TC service"
 Start-Service teamcity
